35123e9a0566d16ecbe628c9b9c8ab751d7f0bc3
Muhammad Aqib Javed <maheraqibofficial@gmail.com>
2025-04-16 18:59:24 +0500

    update newsmonkey

--- src/Components/News.js
+++ src/Components/News.js
@@ -3,21 +3,37 @@ import NewsItem from './NewsItem'
 // import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import NewsSkeleton from './NewsSkeleton';
-
+import PropTypes from 'prop-types'
 export class News extends Component {
+
+  static defaultProps = {
+    country: 'us',
+    pageSize: 6,
+    category: 'general',
+  }
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  }
+
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
  
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             articles: [],
             loading: false,
             page: 1,
         }
+        document.title = `${this.capitalizeFirstLetter (this.props.category)} - NewsMonkey`;
     } 
 
     async componentDidMount() {
         this.setState({ loading: true });
-        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=d1381dfa4e6c4b048f4fbdd9639ce349&page=1&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d1381dfa4e6c4b048f4fbdd9639ce349&page=1&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
@@ -30,7 +46,7 @@ export class News extends Component {
       nextPage = async () => {
         const nextPage = this.state.page + 1;
         this.setState({ loading: true });
-        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=d1381dfa4e6c4b048f4fbdd9639ce349&page=${nextPage}&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d1381dfa4e6c4b048f4fbdd9639ce349&page=${nextPage}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
@@ -43,7 +59,7 @@ export class News extends Component {
       priviousPage = async () => {
         const prevPage = this.state.page - 1;
         this.setState({ loading: true });
-        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=d1381dfa4e6c4b048f4fbdd9639ce349&page=${prevPage}&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d1381dfa4e6c4b048f4fbdd9639ce349&page=${prevPage}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
@@ -59,7 +75,7 @@ export class News extends Component {
     return (
       <div>
         <div className="container my-3 ">
-          <h2 className="text-center">NewsMonkey - Top Headlines</h2>
+          <h2 className="text-center">NewsMonkey - Top {this.capitalizeFirstLetter (this.props.category)} Headlines</h2>
 
           <div className="row">
             {this.state.loading
@@ -69,10 +85,11 @@ export class News extends Component {
               : (this.state.articles || []).map((element) => (
                   <div className="col-md-4" key={element.url}>
                     <NewsItem
-                      title={(element.title || "").slice(0, 50)}
-                      description={(element.description || "").slice(0, 90)}
+                      title={(element.title || "").slice(0, 45)}
+                      description={(element.description || "").slice(0, 88)}
                       imageUrl={element.urlToImage}
-                      newsUrl={element.url}
+                      newsUrl={element.url} author={element.author} date={element.publishedAt}
+                      source={element.source}
                     />
                   </div>
                 ))}
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
       <div className='my-3'>
 
-            <div className="card" style={{width: "18rem"}}>
+            <div className="card">
+            <span className="position-absolute top-0 translate-middle badge rounded-pill bg-primary" style={{left:'85%', zIndex:'1'}}>{source.name}</span>
             <img src={imageUrl || "https://media.istockphoto.com/id/1222357475/vector/image-preview-icon-picture-placeholder-for-website-or-ui-ux-design-vector-illustration.jpg?s=612x612&w=0&k=20&c=KuCo-dRBYV7nz2gbk4J9w1WtTAgpTdznHu55W9FjimE="}  style={{ height: "200px", objectFit: "cover" }} className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title"> {title}... </h5>
+                <h5 className="card-title"> {title}...</h5>
                 <p className="card-text">{description}...</p>
+                <strong><p className="card-text"><small className="text-body-secondary">By {!author? "Unknown" :author} On {new Date(date).toGMTString()}</small></p></strong>
                 <a href= {newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
             </div>
             </div>
